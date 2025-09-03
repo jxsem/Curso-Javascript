e2545d671676a39d9a90d3cdea28934297b10c77
Jose Manuel <soldadojimenezj@gmail.com>
2025-09-03 12:55:36 +0200

    Añadiendo un comentario al ejercicio de borrar un elemento de un array

--- Basic/13-structures-exercise.js
+++ Basic/13-structures-exercise.js
@@ -5,25 +5,34 @@ animales.unshift("Gusano")
 animales.push("saltamontes")
 console.log(animales)
 // 3. Elimina el que se encuentra en tercera posición.
-animales.splice(2, 1) 
+animales.splice(2, 1)  // 2 es el índice de inicio (tercer elemento, porque los índices empiezan en 0). 1 es la cantidad de elementos a eliminar desde ese índice
 console.log(animales)
 // 4. Crea un set que almacene cinco libros.
 let set = new Set(["Cien años de soledad", "1984", "El principito", "Don Quijote de la Mancha","La sombra del viento"])
 // 5. Añade dos más. Uno de ellos repetido.
-set.add(["Clean Code"])
-set.add(["La sombra del viento"])
+set.add("Clean Code")
+set.add("La sombra del viento")
+console.log(set)
 // 6. Elimina uno concreto a tu elección.
 set.delete("Don Quijote de la Mancha")
+console.log(set)
 // 7. Crea un mapa que asocie el número del mes a su nombre.
 let mapa = new Map([
     ["Enero", 1]
     ["Febrero", 2]
     ["Marzo", 3]
     ["Abril", 4]
-    ["Mayo", 5]
-    ["Junio", 6]])
+    ["Mayo", 5]])
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor.
-mapa.has(5)
+if (mapa.has(5)) {
+    console.log(mapa.get(5)); // Mayo
+}
 // 9. Añade al mapa una clave con un array que almacene los meses de verano.
-
+mapa.set("Verano", ["Junio", "Julio", "Agosto"]);
+console.log(mapa);
 // 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map.
+let numerosArray = [1, 2, 2, 3, 4, 4, 5];
+let numerosSet = new Set(numerosArray);
+let mapFinal = new Map();
+mapFinal.set("NumerosUnicos", numerosSet);
+console.log(mapFinal);
