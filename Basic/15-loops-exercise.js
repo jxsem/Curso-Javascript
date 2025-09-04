6482b2f2f9f1c05a8c4ffc8d03f12dcf0a764692
Jose Manuel <soldadojimenezj@gmail.com>
2025-09-04 13:28:05 +0200

    Añadiendo los enunciados de ejercicios sobre loops

---
+++ Basic/15-loops-exercise.js
@@ -0,0 +1,10 @@
+// 1. Imprimir los números del 1 al 20
+// 2. Sumar todos los números del 1 al 100 y mostrar el resultado
+// 3. Imprimir todos los números pares entre 1 y 50
+// 4. Imprimir cada nombre de un array de nombres
+// 5. Contar el número de vocales en una cadena de texto
+// 6. Multiplicar todos los números de un array y mostrar el producto
+// 7. Imprimir la tabla de multiplicar del 5
+// 8. Invertir una cadena de texto
+// 9. Generar los primeros 10 números de la secuencia de Fibonacci
+// 10. Crear un nuevo array con los números mayores a 10 de un array dado
